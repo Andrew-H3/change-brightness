3e7a35bb47abacfb2ac9ec654754e60d8d6110e2
Andrew <26437357+Andrew-H3@users.noreply.github.com>
2018-08-11 10:58:15 -0400

    Program works
    Program now works properly

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@
 default : sb.o
 	gcc sb.o -o sb
 
-sb.o : main.c
-	gcc -c main.c -o sb.o
+sb.o : sb.c
+	gcc -c sb.c -o sb.o
 
 clean : 
 	rm sb.o
--- sb.c
+++ sb.c
@@ -1,47 +1,13 @@
-//#define debug
+#define debug
 #define brightfile "/sys/class/backlight/intel_backlight/brightness"
 #define maxfile "/sys/class/backlight/intel_backlight/max_brightness"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
-	
-	FILE *fp = NULL;
-	float argVal = 0;
-	
-	//check arg
-	if (argv[1] != NULL){
-		sscanf(argv[1], "%f", &argVal);
-		if (argVal > 100) {
-			printf("ERROR: cannot excede 100 percent\n");
-			return 0;}
-#ifdef debug
-		printf("argument given: %f\n", argVal);
-#endif
-	}
-	else {printf("no value given\n"); return 0;}
-
-
-	//convert arg from percent to decimal
-	float newVal = argVal/100;
-#ifdef debug
-	printf("new percent to set: %f\n", newVal);
-#endif
-
-
-	//check if file exists
-	fp = fopen( brightfile, "r");
-	if (fp != NULL) {
-#ifdef debug
-		printf("brightness file exists\n");
-#endif
-		fclose(fp);}
-	else {printf("file does not exist\n"); return 0;}
-
-
 	//check brightness
 int getBrightness(void){
+	FILE *fp = NULL;
 	int brightness = 0;
 	char line[5];
 	fp = fopen( brightfile, "r");
@@ -60,6 +26,7 @@ int getBrightness(void){
 
 	//check max brightness
 int getMaxBrightness(void){
+	FILE *fp = NULL;
 	int maxBrightness = 0;
 	char line[5];
 	fp = fopen( maxfile, "r");
@@ -78,6 +45,7 @@ int getMaxBrightness(void){
 
 	//set brightness
 int setBrightness(int val){
+	FILE *fp = NULL;
 	fp = fopen( brightfile, "w");
 	if (fp != NULL){
 #ifdef debug
@@ -90,9 +58,37 @@ int setBrightness(int val){
 	else {printf("error: could not access file\n"); return -1;}
 }
 
-#ifdef debug
-	printf("made it to end\n");
-#endif
+int main(int argc, char *argv[]){
 	
-	return 0;
+	FILE *fp = NULL;
+	float argVal = 0;
+	
+	//check arg
+	if (argv[1] != NULL){
+		sscanf(argv[1], "%f", &argVal);
+		if (argVal > 100) {
+			printf("ERROR: cannot excede 100 percent\n");
+			return 0;}
+#ifdef debug
+		printf("argument given: %f\n", argVal);
+#endif
+	}
+	else {printf("no value given\n"); return 0;}
+
+#ifdef debug
+	printf("new percent to set: %f\n", argVal/100);
+#endif
+
+
+	//check if file exists
+	fp = fopen( brightfile, "r");
+	if (fp != NULL) {
+#ifdef debug
+		printf("brightness file exists\n");
+#endif
+		fclose(fp);}
+	else {printf("file does not exist\n"); return 0;}
+	
+	setBrightness((int)(getMaxBrightness()*(argVal/100)));
+
 }
